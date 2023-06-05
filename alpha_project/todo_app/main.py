ac98730747e6ef1b5dd7468c05b27a44d277f531
rkwiecien <rkwiecien@ubuntu.myguest.virtualbox.org>
2023-06-05 20:21:42 +0200

    cleanup

--- alpha_project/todo_app/main.py
+++ alpha_project/todo_app/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
